b101a5ad95248157f6124d4a464e2fa444770b98
saramchamness-afk <saramchamness@gmail.com>
2026-03-12 23:35:26 -0700

    Added files to create website via Sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Romi mecha32'
+copyright = '2026, Fred Agregado, Samantha Kenney, Sara Chamness'
+author = 'Fred Agregado, Samantha Kenney, Sara Chamness'
+release = '3/20/2026'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
