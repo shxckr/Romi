08fa86161175b4d5253ffb5a0495bb0ec85dd355
shxckr <fagregad@calpoly.edu>
2026-03-16 13:48:52 -0700

    removed comments

--- linesensors.py
+++ linesensors.py
@@ -6,8 +6,6 @@ class LineSensors:
         self.IN_PINS = IN_PINS
         self.adcs = [pyb.ADC(pyb.Pin(pin)) for pin in IN_PINS]
         self.samples = max(1, int(samples))
-        # self.mins = [1500] * len(self.IN_PINS)
-        # self.maxs = [2500] * len(self.IN_PINS)
         self.mins = [0] * len(self.IN_PINS)
         self.maxs = [4095] * len(self.IN_PINS)
         # line sensors read lower numbers for white
@@ -29,7 +27,6 @@ class LineSensors:
             vals = self.read_raw()   # tuple of 7 values
             for i in range(len(self.IN_PINS)):
                 self.maxs[i] = min(self.maxs[i], vals[i])
-        #print("Dark calibration complete")
         # self.maxs = [1908, 1706, 1725, 1725, 1725, 1369, 1807]
         print(f"maxs: {self.maxs}")
     def _getDark(self):
@@ -42,7 +39,6 @@ class LineSensors:
                 vals = self.read_raw()
                 for i in range(len(self.IN_PINS)):
                     self.mins[i] = max(self.mins[i], vals[i])
-            #print("Light calibration complete")
             print(f"mins: {self.mins}")
     
     def _getLight(self):
@@ -62,15 +58,11 @@ class LineSensors:
 
     def line_error(self, line_is_dark=True):
         norm = self.read_normalized()
-        #print(norm)
-
         # convert to "line strength" (bigger => more on the line)
         if line_is_dark:
             strengths = [1.0 - v for v in norm]
         else:
             strengths = norm
-        # print(strengths)
-
         total = sum(strengths)
         if total < 1e-6:
             return 0.0  # lost line; could also return last error
@@ -78,10 +70,8 @@ class LineSensors:
         # positions across sensor bar: -1 .. +1
         pos_weights = [(i - 3) / 3.0 for i in range(len(self.IN_PINS))]
         pos = sum(w * s for w, s in zip(pos_weights, strengths)) / total
-        #print(pos)
         return pos
     
 
 
     # Yellow pins = ['PA4', 'PB0', 'PB1', 'PC0', 'PC1', 'PC2', 'PC3']
-    # Pink pins = ['PC2','PC3','PC0',"PC1","PB0","PA4","PC_4"]
