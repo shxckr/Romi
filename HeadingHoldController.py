ca59216639d57b51deb8979c9787caa1a0dc4fa6
shxckr <fagregad@calpoly.edu>
2026-03-16 21:56:20 -0700

    added ability for ki control in heading controller

--- HeadingHoldController.py
+++ HeadingHoldController.py
@@ -1,18 +1,19 @@
 class HeadingHoldController:
     def __init__(self, heading_share, initHeadSh, target_heading=180.0,
-                 base_speed=180.0, kp=2.0, min_speed=100.0, max_speed=300.0):
+                 base_speed=180.0, kp=2.0, ki=0,min_speed=100.0, max_speed=300.0):
         self.heading_share = heading_share
         self.target_heading = target_heading
         self.base_speed = base_speed
         self.kp = kp
+        self.ki = ki
         self.min_speed = min_speed
         self.max_speed = max_speed
         self._initHeadSh = initHeadSh
-
+        self._errSum = 0.0
     def _wrap_angle(self, angle):
-        while angle >= 180:
+        if angle >= 180:
             angle -= 360
-        while angle < -180:
+        if angle < -180:
             angle += 360
         return angle
 
@@ -29,8 +30,8 @@ class HeadingHoldController:
         )
 
         error = self._wrap_angle(self.target_heading - heading)
-        correction = self.kp * error
-
+        self._errSum += error
+        correction = self.kp * error + self.ki * self._errSum
         if self.base_speed >= 0:
             # forward driving
             left_speed = self.base_speed + correction
