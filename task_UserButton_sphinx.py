9c937e516b4c74a1f8e3807b9b83c5ac83557074
shxckr <fagregad@calpoly.edu>
2026-03-20 20:29:51 -0700

    Add docstrings to task_UButton class
    Added docstrings to the task_UButton class and its methods for better clarity on functionality and usage.

--- task_UserButton_sphinx.py
+++ task_UserButton_sphinx.py
@@ -26,15 +26,31 @@ S1_Poll = micropython.const(1) # Polling state
 S2_Run = micropython.const(2) # Raise flag state
 
 class task_UButton:
+    '''
+    This task instantiates the user button on the STM32l476RG which is connected to the PA5.
+    When the button is pressed, its value is set low due to its active low configuration and the 
+    flag_share is set to high.
+
+    Arguements:
+    pin: a pyb.Pin object that is connnected to the user button
+    flag_share: a share object that stores whether the button is pressed and is used to 
+                communicate with other tasks 
+    '''
     def __init__(self, pin: Pin, flag_share: Share):
+        '''
+        Initializes the user button connected to Pin and sets the flag_share high when 
+        the user button is pressed.
+        '''
         self._button = pin
         self._flag_share = flag_share
         self._state=S0_INIT
         self._flag_share.put(False)
 
     def update(self):
+        '''Polls the user button once and returns True if the button is pressed'''
         return not self._button.value() == True # active low button, returns True when pressed
     def run(self):
+        '''Runs one instance of the task_UButton'''
         while True:
             if self._state == S0_INIT:
                 self._state = S1_Poll
